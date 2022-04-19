cc38dad77b63550ba1894865214b1f096994105e
L1O <cingwun0817@gmail.com>
2022-04-19 13:00:43 +0800

    fix(TwoSum): used map logic handle

--- two-sum/main.go
+++ two-sum/main.go
@@ -3,27 +3,44 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-	var result []int
+	m := make(map[int]int)
 
-	for index1, value1 := range nums {
-		for index2, value2 := range nums {
-			if index1 != index2 && value1+value2 == target && len(result) == 0 {
+	for i, num := range nums {
+		j, exist := m[target-num]
 
-				result = append(result, index1)
-				result = append(result, index2)
-
-				break
-			}
+		if exist {
+			return []int{i, j}
 		}
+
+		m[num] = i
 	}
 
-	return result
+	return []int{-1, -1}
 }
+
+// func twoSum(nums []int, target int) []int {
+// 	var result []int
+
+// 	for index1, value1 := range nums {
+// 		for index2, value2 := range nums {
+// 			if index1 != index2 && value1+value2 == target && len(result) == 0 {
+
+// 				result = append(result, index1)
+// 				result = append(result, index2)
+
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	return result
+// }
 
 func main() {
 	nums := []int{3, 2, 4}
 	target := 6
 
+	// twoSum(nums, target)
 	result := twoSum(nums, target)
 	fmt.Println(result)
 }
